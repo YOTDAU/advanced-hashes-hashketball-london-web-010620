b8de4d0de5749da6fc2942de6b498d6c0f51ba70
Simon Tucker <sjtucker28@gmail.com>
2020-01-02 12:25:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -163,7 +163,7 @@ end
 def player_numbers(team_name)
   numbers = []
   game_hash.each do |place, team|
-    if team == :team_name
+    if team[:team_name] == :team_name
       team.each do |attributes, data|
         if attributes == :players
           data.each do |player|
