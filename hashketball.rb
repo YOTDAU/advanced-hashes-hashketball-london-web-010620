79cd0dba03910f5e525d62e62b5b57853dd85ad2
Simon Tucker <sjtucker28@gmail.com>
2020-01-02 12:18:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,5 +132,18 @@ def num_points_scored(player_name)
   end
 end
 
-  
+def shoe_size(player_name)
+  game_hash.each do |place, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player_name] == player_name
+            return player[:shoe]
+          end
+        end
+      end
+    end
+  end
+end
+
   
